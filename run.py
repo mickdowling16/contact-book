fcfdb0a6f917d81a1bdbd862bbb280e175981eee
mickdowling16 <michaeldowling16@gmail.com>
2023-03-21 20:27:16 +0000

    Add print statement to return all data from Google sheet

--- run.py
+++ run.py
@@ -11,3 +11,9 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('contact_book')
+
+contact = SHEET.worksheet('contact')
+
+data = contact.get_all_values()
+
+print(data)
